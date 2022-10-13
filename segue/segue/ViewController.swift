9f84322626567c0d8b919bf568b4ec9279e35ec7
saan04 <102419587+saan04@users.noreply.github.com>
2022-10-13 12:27:36 +0530

    Add files via upload

---
+++ segue/segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segue
+//
+//  Created by APPLE on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
